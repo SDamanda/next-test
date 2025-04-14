c5ee0560a2f4baf8c86cdb723e4d6600979a6691
Amanda dos Santos Daros <amandsan08@gmail.com>
2025-04-14 16:10:21 -0300

    1a

---
+++ src/pages/sobre.js
@@ -0,0 +1,11 @@
+export default function Sobre() {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center p-8">
+        <h1 className="text-4xl font-bold mb-4">Sobre mim</h1>
+        <p className="text-lg text-gray-600">
+          Essa é a página de sobre. Aqui posso falar quem sou eu 😄
+        </p>
+      </div>
+    );
+  }
+  
