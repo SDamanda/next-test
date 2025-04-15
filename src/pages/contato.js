3fa5c2a408e8212a349b33066f27825d9eed9dea
Amanda dos Santos Daros <amandsan08@gmail.com>
2025-04-15 13:05:18 -0300

    6a

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -1,12 +1,9 @@
 export default function Contato() {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold text-blue-600">Pagina de Contato</h1>
-            <p className="text-gray-600 mt-4">formulario</p>
-
-            <link href="/src/pages/contato.js" className="text-blue-500 hover:underline">Voltar para Home</link>"
-        </div>
-
+      <div className="min-h-screen flex flex-col items-center justify-center p-8">
+        <h1 className="text-4xl font-bold text-blue-600">Página de Contato</h1>
+        <p className="text-gray-600 mt-4">formulário</p>
+      </div>
     );
-
-}
+  }
+  
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+
 export default function Home() {
+  const router = useRouter()
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-8">
       <h1 className="text-4xl font-bold mb-4">Olá, mundo!</h1>
       <p className="text-lg text-gray-600">ola</p>
 
-      <Link href="" className="text-blue-500 hover:underline">ir para Contato</Link>
-      
+      <p onClick={()=>router.push("/contato")} className="text-blue-500 hover:underline">ir para Contato</p>
+
     </div>
   );
-  
+
 }
