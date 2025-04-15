500456ccf03a770176f85911309681e379777420
Amanda dos Santos Daros <amandsan08@gmail.com>
2025-04-15 19:22:54 -0300

    5a

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import Link from "next/link"
+import styles from '/index.module.css';
 import { useRouter } from "next/router";
 
 
@@ -10,7 +11,14 @@ export default function Home() {
       <p className="text-lg text-gray-600">ola</p>
 
       <p onClick={()=>router.push("/contato")} className="text-blue-500 hover:underline">ir para Contato</p>
-
+      <p onClick={()=>router.push("/produtos")} className="text-blue-500 hover:underline">ir para Produtos</p>
+    </div>
+    
+  );
+  return (
+    <div className={styles.container}>
+      <h1>pagina</h1>
+      <p>conteudo adaptavel para qualquer tela</p>
     </div>
   );
 
